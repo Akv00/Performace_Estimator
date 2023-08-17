3e9e4007ce88a459a49d924c983d4cfba59db312
Akv00 <kunal613.kv@gmail.com>
2023-08-17 12:59:26 +0530

    solved minor issues

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,23 +2,27 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+# import os
 
 class PredictPipeline:
     def __init__(self):
         pass
-    
+
     def predict(self,features):
         try:
             model_path='artifacts\model.pkl'
             preprocessor_path='artifacts\preprocessor.pkl'
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
-
+        
         except Exception as e:
             raise CustomException(e,sys)
+
 
 
 class CustomData:
@@ -30,7 +34,7 @@ class CustomData:
         test_preparation_course: str,
         reading_score: int,
         writing_score: int):
-        
+
         self.gender = gender
 
         self.race_ethnicity = race_ethnicity
